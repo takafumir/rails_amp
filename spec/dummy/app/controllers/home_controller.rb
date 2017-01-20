da2a5893069f808ffbb142a05d9ab788196b5da0
Takafumi Yamano <takafumiyamano@gmail.com>
2017-01-20 17:02:36 +0900

    add home#about for spec dummy

--- spec/dummy/app/controllers/home_controller.rb
+++ spec/dummy/app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
+# Just for test.
 class HomeController < ApplicationController
   def index
+  end
+
+  def about
+    respond_to do |format|
+      format.html  # about.html.erb
+      format.json { render json:
+        { object_id: RailsAmp.config.object_id, format: RailsAmp.config.format }
+      }
+    end
   end
 end
