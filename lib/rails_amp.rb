8d286be8bb9056c6564f89f84d9e1da1319e9ba2
Takafumi Yamano <takafumiyamano@gmail.com>
2017-01-20 15:23:26 +0900

    add module methods to RailsAmp

--- lib/rails_amp.rb
+++ lib/rails_amp.rb
@@ -1,9 +1,36 @@
-module RailsAmp
-  # Your code goes here...
-end
+require 'rails_amp/version'
 
 if defined?(Rails::Railtie)
   require 'rails_amp/railtie'
 else
   raise "rails_amp is not compatible with Rails 2.* or older"
+end
+
+module RailsAmp
+  autoload :Config, 'rails_amp/config'
+
+  extend(Module.new {
+    # Get RailsAmp configuration object
+    def config
+      Thread.current[:rails_amp_config] ||= RailsAmp::Config.new
+    end
+
+    # Set RailsAmp configuration object
+    def config=(value)
+      Thread.current[:rails_amp_config] = value
+    end
+
+    # Write methods which delegates to the configuration object
+    %w(format).each do |method|
+      module_eval <<-DELEGATORS, __FILE__, __LINE__ + 1
+        def #{method}
+          config.#{method}
+        end
+
+        def #{method}=(value)
+          config.#{method} = (value)
+        end
+      DELEGATORS
+    end
+  })
 end
