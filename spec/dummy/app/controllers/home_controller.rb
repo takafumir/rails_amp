f800eff109f8880f3c5165c702f8ce2d9cabb4d1
Takafumi Yamano <takafumiyamano@gmail.com>
2017-01-21 01:13:24 +0900

    update dummy home controller

--- spec/dummy/app/controllers/home_controller.rb
+++ spec/dummy/app/controllers/home_controller.rb
@@ -9,10 +9,33 @@ class HomeController < ApplicationController
       format.json { render json:
         { object_id: RailsAmp.config.object_id, format: RailsAmp.config.format }
       }
-      format.amp do
-        lookup_context.formats = [:amp, :html]  # search .amp .html templates
-        render
-      end
     end
   end
+end
+
+class HomeController
+  before_action :amp_render
+
+  private
+
+    def about_with_amp
+      about_without_amp
+
+      respond_to do |format|
+        format.amp do
+          # search .amp .html templates
+          lookup_context.formats = [:amp, :html]
+          render
+        end
+      end
+    end
+
+    def amp_render
+      if request.format == 'amp'
+        class_eval do
+          alias_method :about_without_amp, :about
+          alias_method :about, :about_with_amp
+        end
+      end
+    end
 end
