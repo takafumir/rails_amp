b341ad2b95d71ebad2522bcdacf63118d4f36c99
Takafumi Yamano <takafumiyamano@gmail.com>
2017-01-23 11:10:37 +0900

    use Module#prepend instead of alia_method

--- spec/dummy/app/controllers/home_controller.rb
+++ spec/dummy/app/controllers/home_controller.rb
@@ -13,28 +13,28 @@ class HomeController < ApplicationController
   end
 end
 
+module Ampify
+  def about
+    super
+    respond_to do |format|
+      format.amp do
+        # search .amp .html templates
+        lookup_context.formats = [:amp, :html]
+        render
+      end
+    end
+  end
+end
+
 class HomeController
   before_action :amp_render
 
   private
 
-    def about_with_amp
-      about_without_amp
-
-      respond_to do |format|
-        format.amp do
-          # search .amp .html templates
-          lookup_context.formats = [:amp, :html]
-          render
-        end
-      end
-    end
-
     def amp_render
       if request.format == 'amp'
-        class_eval do
-          alias_method :about_without_amp, :about
-          alias_method :about, :about_with_amp
+        self.class.class_eval do
+          prepend Ampify
         end
       end
     end
