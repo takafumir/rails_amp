450246ecda889036bf377f888f851c913b7718fa
Takafumi Yamano <takafumiyamano@gmail.com>
2017-01-23 17:15:16 +0900

    update railtie

--- lib/rails_amp/railtie.rb
+++ lib/rails_amp/railtie.rb
@@ -19,24 +19,51 @@ module RailsAmp
       end
     end
 
-    def override_with_amp
-      klass = self.class
-      key = klass.name.underscore.sub(/_controller\z/, '')
-      klass.class_eval do
-        prepend(Module.new {
-          RailsAmp.enables[key].to_a.each do |action|
-            define_method action.to_sym do
-              super()
-              respond_to do |format|
-                format.amp do
-                  # search .amp .html templates
-                  lookup_context.formats = [:amp, :html]
+    private
+
+      def override_with_amp
+        klass = self.class
+        actions = amp_actions(klass)
+        klass.class_eval do
+          prepend(Module.new {
+            actions.to_a.each do |action|
+              define_method action.to_sym do
+                super()
+                respond_to do |format|
+                  format.amp do
+                    # search .amp .html templates
+                    lookup_context.formats = [:amp, :html]
+                  end
                 end
               end
             end
-          end
-        })
+          })
+        end
       end
-    end
+
+      def amp_actions(klass)
+        return []                        if amp_none?
+        return klass.action_methods.to_a if amp_all?
+        key = klass.name.underscore.sub(/_controller\z/, '')
+        return klass.action_methods.to_a if amp_controller_all?(key)
+        return RailsAmp.enables[key]     if amp_controller_actions?(key)
+        []
+      end
+
+      def amp_none?
+        RailsAmp.enables == { 'application' => ['none'] }
+      end
+
+      def amp_all?
+        RailsAmp.enables == { 'application' => ['all'] }
+      end
+
+      def amp_controller_all?(key)
+        RailsAmp.enables.has_key?(key) && RailsAmp.enables[key].blank?
+      end
+
+      def amp_controller_actions?(key)
+        RailsAmp.enables.has_key?(key) && RailsAmp.enables[key].present?
+      end
   end
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  get '/index', to: 'home#index'
-  get '/help',  to: 'home#help'
-  get '/about', to: 'home#about'
+  get '/home/index', to: 'home#index'
+  get '/home/help',  to: 'home#help'
+  get '/home/about', to: 'home#about'
 
   get '/users/index', to: 'users#index'
   get '/users/show',  to: 'users#show'
