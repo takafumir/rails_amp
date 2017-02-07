462c6749f3bdc04c9598ed600fe8033c8710bf34
Takafumi Yamano <takafumiyamano@gmail.com>
2017-02-07 17:22:59 +0900

    limit dummy app frameworks

--- spec/dummy/app/assets/javascripts/cable.js
+++ spec/dummy/app/assets/javascripts/cable.js
@@ -1,7 +1,6 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the rails generate channel command.
 //
-//= require action_cable
 //= require_self
 //= require_tree ./channels
 
