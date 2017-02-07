9c66c445b8875a84e779fe4e9ea6d376595780cd
Takafumi Yamano <takafumiyamano@gmail.com>
2017-02-07 17:57:11 +0900

    disable dummy app some initializers

--- spec/dummy/config/initializers/new_framework_defaults.rb
+++ spec/dummy/config/initializers/new_framework_defaults.rb
@@ -5,10 +5,10 @@
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
 # Enable per-form CSRF tokens. Previous versions had false.
-Rails.application.config.action_controller.per_form_csrf_tokens = true
+# Rails.application.config.action_controller.per_form_csrf_tokens = true
 
 # Enable origin-checking CSRF mitigation. Previous versions had false.
-Rails.application.config.action_controller.forgery_protection_origin_check = true
+# Rails.application.config.action_controller.forgery_protection_origin_check = true
 
 # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
 # Previous versions had false.
